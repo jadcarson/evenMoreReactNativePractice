611e4bb15322ba40982a91b985314809bfa69f6a
Jad Carson <Jad.Carson@gmail.com>
2016-10-31 14:24:41 -0500

    added the baseline of employee list screen and the ability to switch between the views

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -15,7 +15,7 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: '', loading: false };
+      return { ...state, user: action.payload, error: '', loading: false, email: '', password: '' };
     case LOGIN_USER_FAIL:
       return { ...state, error: 'Authentication Failed.', password: '', loading: false };
     case LOGIN_USER:
